bcd06f931de768cb109b03ef9e7c9781266a3d4a
CHRISTOPHER TRUONG <christophertruongm@gmail.com>
2019-01-23 18:31:21 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,25 @@
 #write your code here
 
-def countdown
+def countdown(n)
+  currentCount = n
+  
+  while currentCount > 0
+    puts "#{currentCount} SECOND(S)!"
+    currentCount -= 1
+  end
+  
+  return "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(n)
+  currentCount = n
+  
+    while currentCount > 0
+    puts "#{currentCount} SECOND(S)!"
+    currentCount -= 1
+    sleep(1)
+  end
+  
+  return "HAPPY NEW YEAR!"
+  
 end
